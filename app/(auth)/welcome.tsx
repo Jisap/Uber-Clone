53239c9f3406c88ee3d8188d9b5ccc81ce5b26b0
Jorge Matos <jisapjisap@gmail.com>
2024-08-21 18:11:55 +0200

    signUp & inputField

--- app/(auth)/welcome.tsx
+++ app/(auth)/welcome.tsx
@@ -4,11 +4,14 @@ import { useRef, useState } from "react";
 import { SafeAreaView } from "react-native-safe-area-context";
 import Swiper from "react-native-swiper";
 import { onboarding } from '../../constants/index';
+import CustomButton from "@/components/CustomButton";
 
 const OnBoarding = () => {
   
   const swiperRef = useRef<Swiper>(null);
-  const [activeIndex, setActiveIndex] = useState(0)
+  const [activeIndex, setActiveIndex] = useState(0);
+  const isLastSlide = activeIndex === onboarding.length - 1;
+
   
   return (
     <SafeAreaView className="flex h-full items-center justify-between bg-white">
@@ -33,12 +36,27 @@ const OnBoarding = () => {
               className="w-full h-[300px]"
               resizeMode="contain"
             />
-            <Text>
-              {item.title}
+            <View className="flex flex-row items-center justify-center w-full mt-10">
+              <Text className="text-black text-3xl font-bold mx-10 text-center">
+                {item.title}
+              </Text>
+            </View>
+            <Text className="text-lg font-JakartaSemiBold text-center text-[#858585] mx-10 mt-3">
+              {item.description}
             </Text>
           </View>
         ))}
       </Swiper>
+
+      <CustomButton 
+        title={isLastSlide ? "Get Started" : "Next"}
+        onPress={() =>
+          isLastSlide
+            ? router.replace("/(auth)/sign-up")
+            : swiperRef.current?.scrollBy(1)
+        }
+        className="w-11/12 mt-10"
+      />
 
     </SafeAreaView>
   )
