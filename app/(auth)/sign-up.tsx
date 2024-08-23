c964546fd7271ba37b8543c3a0dd73da6b493ee0
Jorge Matos <jisapjisap@gmail.com>
2024-08-23 17:51:02 +0200

    fix

--- app/(auth)/sign-up.tsx
+++ app/(auth)/sign-up.tsx
@@ -6,10 +6,20 @@ import { useState } from "react";
 import CustomButton from "@/components/CustomButton";
 import { Link } from "expo-router";
 import OAuth from "@/components/Oauth";
+import { useSignUp } from "@clerk/clerk-expo";
+import { useRouter } from 'expo-router'
+import ReactNativeModal from "react-native-modal";
 
 
 const SignUp = () => {
 
+  const { isLoaded, signUp, setActive } = useSignUp();
+
+  const [verification, setVerification] = useState({
+    state: 'success',
+    error: '',
+    code: '',
+  });
 
   const [form, setForm] = useState({
     name: "",
@@ -17,15 +27,65 @@ const SignUp = () => {
     password: "",
   });
 
-  const onSignUpPress = async () => {
 
+  const onSignUpPress = async () => {
+    if (!isLoaded) {
+      return
+    }
+
+    try {
+      await signUp.create({
+        emailAddress: form.email,
+        password: form.password,
+      })
+
+      await signUp.prepareEmailAddressVerification({ strategy: 'email_code' })
+
+      setVerification({
+        ...verification,
+        state: 'pending'
+      })
+    } catch (err: any) {
+      console.error(JSON.stringify(err, null, 2))
+    }
+  }
+
+  const onPressVerify = async () => {
+    if (!isLoaded) return
+
+    try {
+      const completeSignUp = await signUp.attemptEmailAddressVerification({
+        code: verification.code,
+      })
+
+      if (completeSignUp.status === 'complete') {
+        //TODO: Create a database user
+        await setActive({ session: completeSignUp.createdSessionId })
+        setVerification({
+          ...verification,
+          state: 'success'
+        })
+      } else {
+        setVerification({
+          ...verification,
+          error: 'Verification failed',
+          state: 'failed'
+        })
+      }
+    } catch (err: any) {
+      setVerification({
+        ...verification,
+        error: err.errors[0].longMessage,
+        state: 'failed',
+      })
+    }
   }
 
   return (
     <ScrollView className="flex-1 bg-white">
       <View className="flex-1 bg-white">
         <View className="relative w-full h-[250px]">
-          <Image 
+          <Image
             source={images.signUpCar}
             className="z-0 w-full h-[250px]"
           />
@@ -35,7 +95,7 @@ const SignUp = () => {
         </View>
 
         <View className="p-5">
-          <InputField 
+          <InputField
             label="Name"
             placeholder="Enter your name"
             icon={icons.person}
@@ -79,10 +139,20 @@ const SignUp = () => {
         </View>
 
         {/* Verification modal */}
+        <ReactNativeModal
+          isVisible={verification.state === 'success'}
+        >
+          <View className="bg-white px-7 py-9 rounded-2xl min-h-[300px]">
+            <Image 
+              source={images.check}
+              className="w-[110px] h-[110px] mx-auto my-5" 
+            />
+          </View>
+        </ReactNativeModal>
 
       </View>
     </ScrollView>
   )
-}
 
+}
 export default SignUp;
--- app/index.tsx
+++ app/index.tsx
@@ -1,9 +1,17 @@
+import { useAuth } from "@clerk/clerk-expo";
 import { Redirect } from "expo-router";
-import { Text, View } from "react-native"
-import { SafeAreaView } from "react-native-safe-area-context";
 
-const Home = () => {
+
+const Page = () => {
+
+  const { isSignedIn } = useAuth();
+
+  if (isSignedIn) {
+    return <Redirect href={'/(root)/(tabs)/home'} />
+  }
+
+
   return <Redirect href="/(auth)/welcome" />
 }
 
-export default Home;
+export default Page;
