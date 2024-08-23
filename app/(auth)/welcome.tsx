e67e09d42f7a63702760208de092a2a77388a025
Jorge Matos <jisapjisap@gmail.com>
2024-08-23 17:31:34 +0200

    clerk

--- app/(auth)/welcome.tsx
+++ app/(auth)/welcome.tsx
@@ -29,8 +29,8 @@ const OnBoarding = () => {
         activeDot={<View className="w-[32px] h-[4px] mx-1 bg-[#0286FF] rounded-full" />}  
         onIndexChanged={(index) => setActiveIndex(index)}
       >
-        {onboarding.map((item) => (
-          <View className="flex items-center justify-center p-5">
+        {onboarding.map((item, index) => (
+          <View className="flex items-center justify-center p-5" key={index}>
             <Image 
               source={item.image}
               className="w-full h-[300px]"
