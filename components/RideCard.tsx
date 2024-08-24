96b82574626e9b6a90bf51c2f87368696d2524eb
Jorge Matos <jisapjisap@gmail.com>
2024-08-24 18:36:37 +0200

    RideCard

--- components/RideCard.tsx
+++ components/RideCard.tsx
@@ -1,5 +1,7 @@
 import { Image, Text, View } from 'react-native'
 import { Ride } from '@/types/type'
+import { icons } from '@/constants'
+import { formatDate, formatTime } from '@/lib/utils'
 
 
 
@@ -16,6 +18,7 @@ const RideCard = ({ ride: {
   return (
     <View className="flex flex-row items-center justify-center bg-white rounded-lg shadow-sm shadow-neutral-300 mb-3">
       <View className="flex flex-col items-start justify-center p-3">
+        
         <View className="flex flex-row items-center justify-between">
           <Image
             source={{
@@ -24,10 +27,35 @@ const RideCard = ({ ride: {
             className="w-[80px] h-[90px] rounded-lg"
           />
 
-          
+          <View className="flex flex-col mx-5 gap-y-5 flex-1">
+            <View className="flex flex-row items-center gap-x-2">
+              <Image source={icons.to} className="w-5 h-5" />
+              <Text className="text-md font-JakartaMedium" numberOfLines={1}>
+                {origin_address}
+              </Text>
+            </View>
+
+            <View className="flex flex-row items-center gap-x-2">
+              <Image source={icons.point} className="w-5 h-5" />
+              <Text className="text-md font-JakartaMedium" numberOfLines={1}>
+                {destination_address}
+              </Text>
+            </View>
+          </View>
         </View>
+
+        <View className="flex flex-col w-full mt-5 bg-general-500 rounded-lg p-3 items-start justify-center">
+          <View className="flex flex-row items-center w-full justify-between mb-5">
+            <Text className="text-md font-JakartaMedium text-gray-500">
+              Date & Time
+            </Text>
+            <Text className="text-md font-JakartaBold" numberOfLines={1}>
+              {formatDate(created_at)}, {formatTime(ride_time)}
+            </Text>
+          </View>
+        </View>
+
       </View>
-      
     </View>
   )
 }
