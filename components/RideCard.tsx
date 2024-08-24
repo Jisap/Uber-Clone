be8ad19f1cd7efe2ef40f3711809cf72f58bef45
Jorge Matos <jisapjisap@gmail.com>
2024-08-24 18:38:49 +0200

    RideCard

--- components/RideCard.tsx
+++ components/RideCard.tsx
@@ -53,6 +53,35 @@ const RideCard = ({ ride: {
               {formatDate(created_at)}, {formatTime(ride_time)}
             </Text>
           </View>
+
+          <View className="flex flex-row items-center w-full justify-between mb-5">
+            <Text className="text-md font-JakartaMedium text-gray-500">
+              Driver
+            </Text>
+            <Text className="text-md font-JakartaBold">
+              {driver.first_name} {driver.last_name}
+            </Text>
+          </View>
+
+          <View className="flex flex-row items-center w-full justify-between mb-5">
+            <Text className="text-md font-JakartaMedium text-gray-500">
+              Car Seats
+            </Text>
+            <Text className="text-md font-JakartaBold">
+              {driver.car_seats}
+            </Text>
+          </View>
+
+          <View className="flex flex-row items-center w-full justify-between">
+            <Text className="text-md font-JakartaMedium text-gray-500">
+              Payment Status
+            </Text>
+            <Text
+              className={`text-md capitalize font-JakartaBold ${payment_status === "paid" ? "text-green-500" : "text-red-500"}`}
+            >
+              {payment_status}
+            </Text>
+          </View>
         </View>
 
       </View>
