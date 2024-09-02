e0d4c232e0cc3e63612b65d7f3a7ad215c8b59fc
Jorge Matos <jisapjisap@gmail.com>
2024-09-02 19:01:52 +0200

    stripe

--- app/(api)/ride/[id]+api.ts
+++ app/(api)/ride/[id]+api.ts
@@ -1,11 +1,19 @@
 import { neon } from "@neondatabase/serverless";
 
-export async function GET(request: Request, { id }: { id: string }) {
-  if (!id)
+export async function GET(request: Request, { id }: { id: string }) {               // Maneja las solicitudes GET para obtener el historial de viajes de un usuario específico.
+  if (!id)                                                                          // Se verifica que se haya proporcionado un ID de usuario.  
     return Response.json({ error: "Missing required fields" }, { status: 400 });
 
   try {
-    const sql = neon(`${process.env.DATABASE_URL}`);
+    const sql = neon(`${process.env.DATABASE_URL}`);            // Se crea una conexión a la base de datos Neon usando la URL almacenada en las variables de entorno.
+
+    // Se ejecuta una consulta SQL compleja que :
+    // 1º Selecciona todos los detalles relevantes de la tabla rides 
+    // 2º Hace un JOIN con la tabla drivers para obtener información del conductor
+    // 3º Construye un objeto JSON con la información del conductor usando json_build_object
+    // 4º Filtra los viajes por el ID de usuario proporcionado.
+    // 5º Ordena los resultados por fecha de creación en orden descendente (más recientes primero).
+
     const response = await sql`
         SELECT
             rides.ride_id,
